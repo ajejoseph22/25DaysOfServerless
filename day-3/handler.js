9f56b8ad853a1434742acb810b6d402bc7b1edbc
Joseph Aje <ajejoseph22@gmail.com>
2019-12-15 00:42:29 +0100

    re-added response wrapper

--- day-3/handler.js
+++ day-3/handler.js
@@ -1,8 +1,18 @@
-module.exports.storeImages = async (event, context, callback) => {
+module.exports.storeImages = (event, context, callback) => {
   const uuid = require("uuid");
   const AWS = require("aws-sdk");
 
   const dynamoDB = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
+
+  const respond = (result, statusCode) => ({
+    statusCode,
+    body: JSON.stringify(result),
+    headers: {
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    }
+  });
 
   const saveImageInDB = item => {
     const params = {
@@ -15,23 +25,24 @@ module.exports.storeImages = async (event, context, callback) => {
 
     dynamoDB.put(params, (err, data) => {
       if (err) {
-        callback(err, null);
-      } else {
-        callback(null, data);
+        callback(err, respond(err, 404));
       }
+      callback(null, respond(data, 201));
     });
   };
 
+  console.log("EVENT", event);
   const body = JSON.parse(event.body);
   const addedItems = body.head_commit.added;
   const extension = ".png";
 
-  addedItems.map(item => {
-    if (item.toLowerCase().endsWith(extension)) {
-      console.log("FOUND...now executing");
-      saveImageInDB(item);
-    }
+  addedItems &&
+    addedItems.map(item => {
+      if (item.toLowerCase().endsWith(extension)) {
+        const link = `https://raw.githubusercontent.com/${body.repository.full_name}/master/${item}`;
+        saveImageInDB(link);
+      }
+    });
 
-    callback(null, "No PNG received");
-  });
+  callback(null, respond("No PNG received", 201));
 };
