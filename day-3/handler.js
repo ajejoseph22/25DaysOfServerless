f7ce671c10f3aa59ab1cbd646ec31e070883b83c
Joseph Aje <ajejoseph22@gmail.com>
2019-12-14 12:01:06 +0100

    set up lambda

--- day-3/handler.js
+++ day-3/handler.js
@@ -1,3 +1,14 @@
 module.exports.storeImages = async event => {
-  return console.log("PAYLOAD", event);
+  const { saveImageInDB } = require("./dynamodb-actions/index");
+  console.log("EVENT", event);
+  const addedItems = event.body.added;
+  const extension = ".png";
+
+  return addedItems.map(item => {
+    if (item.toLowerCase().endsWith(extension)) {
+      saveImageInDB(item)
+        .then(res => res)
+        .catch(err => err);
+    }
+  });
 };
