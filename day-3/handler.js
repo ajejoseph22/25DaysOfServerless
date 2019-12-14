26a178ce49b982e98a497abf4fdd70cd2d4997ff
Joseph Aje <ajejoseph22@gmail.com>
2019-12-14 13:22:38 +0100

    renamed image to trigger lambda

--- day-3/handler.js
+++ day-3/handler.js
@@ -1,28 +1,15 @@
 module.exports.storeImages = async event => {
-  const respond = (result, statusCode) => {
-    return {
-      statusCode,
-      body: JSON.stringify(result),
-      headers: {
-        "Access-Control-Allow-Credentials": true,
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json"
-      }
-    };
-  };
-
   const { saveImageInDB } = require("./dynamodb-actions/index");
   const body = JSON.parse(event.body);
   const addedItems = body.head_commit.added;
-  // console.log("ADDED", body.repository.added);
-  // console.log("ADDED", addedItems);
   const extension = ".png";
 
   return addedItems.map(item => {
     if (item.toLowerCase().endsWith(extension)) {
+      console.log("FOUND...now executing");
       saveImageInDB(item)
-        .then(res => respond(res, 200))
-        .catch(err => respond(err, 404));
+        .then(res => res)
+        .catch(err => err);
     }
   });
 };
