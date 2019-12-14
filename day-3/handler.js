88737f80dbd9a63c65257477ddae045c716df5d3
Joseph Aje <ajejoseph22@gmail.com>
2019-12-14 21:18:37 +0100

    working version

--- day-3/handler.js
+++ day-3/handler.js
@@ -1,19 +1,37 @@
-module.exports.storeImages = async event => {
-  const { saveImageInDB } = require("./dynamodb-actions/index");
+module.exports.storeImages = async (event, context, callback) => {
+  const uuid = require("uuid");
+  const AWS = require("aws-sdk");
+
+  const dynamoDB = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
+
+  const saveImageInDB = item => {
+    const params = {
+      TableName: "images",
+      Item: {
+        id: uuid(),
+        item
+      }
+    };
+
+    dynamoDB.put(params, (err, data) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, data);
+      }
+    });
+  };
+
   const body = JSON.parse(event.body);
   const addedItems = body.head_commit.added;
   const extension = ".png";
 
-  return addedItems.map(async item => {
+  addedItems.map(item => {
     if (item.toLowerCase().endsWith(extension)) {
       console.log("FOUND...now executing");
-      try {
-        await saveImageInDB(item);
-        return "success";
-      } catch (err) {
-        console.log("ERROR", err);
-        return err;
-      }
+      saveImageInDB(item);
     }
+
+    callback(null, "No PNG received");
   });
 };
