7ade7092c1b244b40ee0f08251e13192e2c8e9fe
Joseph Aje <ajejoseph22@gmail.com>
2019-12-14 12:34:17 +0100

    added `respond` method to wrap responses

--- day-3/handler.js
+++ day-3/handler.js
@@ -1,14 +1,27 @@
 module.exports.storeImages = async event => {
+  const respond = (result, statusCode) => {
+    return {
+      statusCode,
+      body: JSON.stringify(result),
+      headers: {
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      }
+    };
+  };
+
   const { saveImageInDB } = require("./dynamodb-actions/index");
-  console.log("EVENT", event);
-  const addedItems = JSON.parse(event.body).added;
+  const body = JSON.parse(event.body);
+  console.log("BODY", body);
+  const addedItems = body.added;
   const extension = ".png";
 
   return addedItems.map(item => {
     if (item.toLowerCase().endsWith(extension)) {
       saveImageInDB(item)
-        .then(res => res)
-        .catch(err => err);
+        .then(res => respond(res, 200))
+        .catch(err => respond(err, 404));
     }
   });
 };
