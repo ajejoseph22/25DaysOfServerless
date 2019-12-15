de27efe1b7cce3c1786569164ca6a360e9385856
Joseph Aje <ajejoseph22@gmail.com>
2019-12-15 05:07:44 +0100

    completed day 4 challenge

---
+++ day-4/handler.js
@@ -0,0 +1,73 @@
+const AWS = require("aws-sdk");
+const uuid = require("uuid");
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
+const baseParams = {
+  TableName: "dishes"
+};
+
+const respond = (result, statusCode) => ({
+  statusCode,
+  body: JSON.stringify(result)
+});
+
+module.exports.listDishes = async () => {
+  const response = await dynamoDB.scan(baseParams).promise();
+
+  return respond(response.Items, 200);
+};
+
+module.exports.addUpdateDish = async event => {
+  const body = JSON.parse(event.body);
+  const { id, dish } = body;
+  const adding = !id;
+
+  const addOrUpdate = async (dish, id) => {
+    const params = {
+      ...baseParams,
+      Item: {
+        id: adding ? uuid() : id,
+        dish
+      }
+    };
+
+    await dynamoDB.put(params).promise();
+  };
+
+  try {
+    await addOrUpdate(dish, id);
+
+    return respond(
+      `Successfuly ${adding ? "Added!" : "Updated!"}`,
+      adding ? 201 : 200
+    );
+  } catch (err) {
+    console.log("ERROR", err);
+    return respond(err, 500);
+  }
+};
+
+module.exports.deleteDish = async event => {
+  const body = JSON.parse(event.body);
+  const { id } = body;
+
+  const remove = async id => {
+    const params = {
+      ...baseParams,
+      Key: {
+        id
+      }
+    };
+
+    await dynamoDB.delete(params).promise();
+  };
+
+  try {
+    await remove(id);
+
+    return respond("Successfully deleted!", 200);
+  } catch (err) {
+    console.log("ERROR", err);
+    return respond(err, 500);
+  }
+};
